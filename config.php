1072c241760ddf0f9ad8169a5abce1acba5e31f2
K.Takahashi <nike.taka@gmail.com>
2016-03-07 01:34:47 +0900

    設定ファイル追加

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DSN', 'mysql:host=localhost;dbname=login_system;charset=utf8');
+define('USER', 'testuser');
+define('PASSWORD', '9999');
+
+// エラーレベルの設定
+error_reporting(E_ALL & ~E_NOTICE);
